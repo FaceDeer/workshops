38557bd423da29fd79389bd9756865f6aaf6514c
FaceDeer <derksenmobile@gmail.com>
2017-05-04 01:31:02 -0600

    switch to a button-based UI

--- smelter.lua
+++ smelter.lua
@@ -1,8 +1,15 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
-local function get_formspec(item_percent, fuel_percent)
-	return table.concat({
+local function refresh_formspec(meta, item_percent, fuel_percent)
+
+	-- TODO: temporary, recalculate from metadata
+	if item_percent == nil then item_percent = 0 end
+	if fuel_percent == nil then fuel_percent = 0 end
+
+	local product_list = minetest.deserialize(meta:get_string("product_list"))
+	
+	local formspec = table.concat({
 		"size[10,9.2]",
 		default.gui_bg,
 		default.gui_bg_img,
@@ -12,21 +19,46 @@ local function get_formspec(item_percent, fuel_percent)
 		"list[context;fuel;0,2.75;4,2]",
 		
 		"image[4.5,0.7;1,1;gui_furnace_arrow_bg.png^[lowpart:"..(item_percent)..":gui_furnace_arrow_fg.png^[transformR270]",
-		"list[context;target;4.5,2;1,1;]",
 		"image[4.5,3.3;1,1;default_furnace_fire_bg.png^[lowpart:"..(fuel_percent)..":default_furnace_fire_fg.png]",
 
 		"list[context;output;6,0.25;4,2;]",
-		"list[context;products;6,2.75;4,2;]",
 
 		"list[current_player;main;1,5;8,1;0]",
 		"list[current_player;main;1,6.2;8,3;8]",
 	})
+
+	local target = meta:get_string("target_item")
+	if target ~= "" then
+		formspec = formspec .. "item_image_button[4.5,2;1,1;" .. target .. ";target;]"
+	else
+		formspec = formspec .. "item_image_button[4.5,2;1,1;;;]"
+	end
+
+	for i = 1,8 do
+		if product_list[i] then
+			formspec = formspec .. "item_image_button[" ..
+			6 + (i-1)%4 .. "," .. 2.75 + math.floor((i-1)/4) ..
+			";1,1;" .. product_list[i] .. ";product_".. i ..";]"
+		else
+			formspec = formspec .. "item_image_button[" ..
+			6 + (i-1)%4 .. "," .. 2.75 + math.floor((i-1)/4) ..
+			";1,1;;product_".. i ..";]"
+		end
+	end
+
+	meta:set_string("formspec", formspec)
 end
 
-local function refresh_products(inv)
-	local craftable = crafting.get_craftable_items("smelter", inv:get_list("input"))
-	inv:set_list("products", craftable)
+local function refresh_products(meta)
+	local inv = meta:get_inventory()
+	local craftable = crafting.get_craftable_items("smelter", inv:get_list("input"), false, true)
+	local product_list = {}
+	for _, craft in pairs(craftable) do
+		table.insert(product_list, craft:get_name())
+	end
+	meta:set_string("product_list", minetest.serialize(product_list))
 end
+
 
 local function smelter_timer(pos, elapsed)
 	local meta = minetest.get_meta(pos)
@@ -36,13 +68,15 @@ local function smelter_timer(pos, elapsed)
 	local burn_time = meta:get_float("burn_time") or 0.0
 	local fuel_time = meta:get_float("fuel_time") or 0.0
 
+	local target_item = meta:get_string("target_item")
+
 	cook_time = cook_time + elapsed
 	burn_time = burn_time - elapsed
 	local cook_percent = 0
 	
 	local recipe
-	if not inv:is_empty("target") then
-		recipe = crafting.get_crafting_result("smelter", inv:get_list("input"), inv:get_stack("target", 1))
+	if target_item ~= "" then
+		recipe = crafting.get_crafting_result("smelter", inv:get_list("input"), ItemStack({name=target_item, count=1}))
 		if not recipe or not crafting.room_for_items(inv, "output", crafting.count_list_add(recipe.output, recipe.returns)) then
 			recipe = nil
 		end
@@ -61,11 +95,9 @@ local function smelter_timer(pos, elapsed)
 					crafting.add_items(inv, "output", output)
 					crafting.remove_items(inv, "input", recipe.input)
 					cook_time = cook_time - recipe.time
-					minetest.debug("items outputted")
 				else
 					-- burn some fuel, if possible.
 					local fuel_recipes = crafting.get_fuels(inv:get_list("fuel"))
-					minetest.debug("recipes", dump(fuel_recipes))
 					local longest_burning
 					for _, fuel_recipe in pairs(fuel_recipes) do
 						if longest_burning == nil or longest_burning.burntime < fuel_recipe.burntime then
@@ -74,7 +106,6 @@ local function smelter_timer(pos, elapsed)
 					end
 					
 					if longest_burning then
-						minetest.debug("longest_burning",dump(longest_burning))
 						fuel_time = longest_burning.burntime
 						burn_time = burn_time + fuel_time
 						inv:remove_item("fuel", ItemStack({name = longest_burning.name, count = 1}))
@@ -98,7 +129,7 @@ local function smelter_timer(pos, elapsed)
 	
 	minetest.debug("Cook time, burn time", tostring(cook_time), tostring(burn_time))	
 
-	meta:set_string("formspec", get_formspec(cook_percent, 0))
+	refresh_formspec(meta, cook_percent, 0)
 
 	meta:set_float("burn_time", burn_time)
 	meta:set_float("fuel_time", fuel_time)
@@ -122,16 +153,8 @@ local function allow_metadata_inventory_put(pos, listname, index, stack, player)
 		else
 			return 0
 		end
-	elseif listname == "target" then
-		if crafting.is_possible_output("smelter", stack:get_name()) then
-			--place an imaginary item into the target slot
-			local inv = minetest.get_inventory({type="node", pos=pos})
-			inv:set_stack(listname, index, stack:take_item(1))
-			smelter_timer(pos, 0)
-		end
-		return 0
-	elseif listname == "products" or listname == "output" then
-		-- not allowed to put items into the products display
+	elseif listname == "output" then
+		-- not allowed to put items into the output
 		return 0
 	end
 	return stack:get_count()
@@ -146,9 +169,6 @@ local function allow_metadata_inventory_take(pos, listname, index, stack, player
 		local inv = minetest.get_inventory({type="node", pos=pos})
 		inv:set_stack(listname, index, ItemStack(""))
 		return 0
-	elseif listname == "products" then
-		-- not allowed to take products directly from the products display
-		return 0		
 	end
 	return stack:get_count()
 end
@@ -172,15 +192,16 @@ minetest.register_node("workshops:smelter", {
 	paramtype2 = "facedir",
 	is_ground_content = false,
 	groups = {oddly_breakable_by_hand = 1, cracky=3},
+
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
 		inv:set_size("input", 2*4) -- materials that can be processed to outputs
 		inv:set_size("fuel", 2*4) -- materials that can be burned for fuel
-		inv:set_size("target", 1) -- Item to actually make from the inputs
-		inv:set_size("products", 2*4) -- potential products from the given inputs
 		inv:set_size("output", 2*4) -- holds output product
-		meta:set_string("formspec", get_formspec(0,0))
+		meta:set_string("product_list", minetest.serialize({}))
+		meta:set_string("target", "")
+		refresh_formspec(meta, 0, 0)
 	end,
 	
 	allow_metadata_inventory_put = allow_metadata_inventory_put,	
@@ -190,25 +211,24 @@ minetest.register_node("workshops:smelter", {
 	on_metadata_inventory_move = function(pos, flist, fi, tlist, ti, no, player)
 		local meta = minetest.get_meta(pos)
 		if tlist == "input" then
-			refresh_products(meta:get_inventory())
-		elseif flist == "output" then
-			--smelter_timer(pos, 0)
+			refresh_products(meta)
 		end
 	end,
 
 	on_metadata_inventory_take = function(pos, lname, i, stack, player)
 		local meta = minetest.get_meta(pos)
 		if lname == "input" then
-			refresh_products(meta:get_inventory())
+			refresh_products(meta)
+			refresh_formspec(meta, 0, 0)
 		elseif lname == "output" then
-			--smelter_timer(pos, 0)
+			smelter_timer(pos, 0)
 		end
 	end,
 
 	on_metadata_inventory_put = function(pos, lname, i, stack, player)
 		local meta = minetest.get_meta(pos)
 		if lname == "input" then
-			refresh_products(meta:get_inventory())
+			refresh_products(meta)
 		end
 		smelter_timer(pos, 0)
 	end,
@@ -219,6 +239,41 @@ minetest.register_node("workshops:smelter", {
 		return inv:is_empty("output") and inv:is_empty("fuel") and inv:is_empty("input")
 	end,
 	
+	on_receive_fields = function(pos, formname, fields, sender)
+		local meta = minetest.get_meta(pos)
+		local product_list = minetest.deserialize(meta:get_string("product_list"))
+		if fields.target ~= nil then
+			meta:set_string("target_item", "")
+			refresh_formspec(meta)
+		elseif fields.product_1 ~= nil then
+			meta:set_string("target_item", product_list[1])
+			refresh_formspec(meta)
+		elseif fields.product_2 ~= nil then
+			meta:set_string("target_item", product_list[2])
+			refresh_formspec(meta)
+		elseif fields.product_3 ~= nil then
+			meta:set_string("target_item", product_list[3])
+			refresh_formspec(meta)
+		elseif fields.product_4 ~= nil then
+			meta:set_string("target_item", product_list[4])
+			refresh_formspec(meta)
+		elseif fields.product_5 ~= nil then
+			meta:set_string("target_item", product_list[5])
+			refresh_formspec(meta)
+		elseif fields.product_6 ~= nil then
+			meta:set_string("target_item", product_list[6])
+			refresh_formspec(meta)
+		elseif fields.product_7 ~= nil then
+			meta:set_string("target_item", product_list[7])
+			refresh_formspec(meta)
+		elseif fields.product_8 ~= nil then
+			meta:set_string("target_item", product_list[8])
+			refresh_formspec(meta)
+		end
+		smelter_timer(pos, 0)
+
+	end,
+
 	on_timer = smelter_timer,
 })
 
