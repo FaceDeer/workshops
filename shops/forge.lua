a019c74d1778659069a988a47310f473e23213af
FaceDeer <derksenmobile@gmail.com>
2017-05-13 16:28:48 -0600

    some forge nodeboxes

--- shops/forge.lua
+++ shops/forge.lua
@@ -3,8 +3,20 @@ local S, NS = dofile(MP.."/intllib.lua")
 
 minetest.register_node("workshops:anvil", {
 	description = S("Anvil"),
-	tiles = {"image.png"},
+	tiles = {"default_furnace_top.png"},
 	groups = {workshops_forge = 2, oddly_breakable_by_hand = 1},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5,-0.5,-0.3,0.5,-0.4,0.3},
+			{-0.35,-0.4,-0.25,0.35,-0.3,0.25},
+			{-0.3,-0.3,-0.15,0.3,-0.1,0.15},
+			{-0.35,-0.1,-0.2,0.35,0.1,0.2},
+		},
+	},
 })
 
 minetest.register_node("workshops:trip_hammer", {
@@ -15,8 +27,23 @@ minetest.register_node("workshops:trip_hammer", {
 
 minetest.register_node("workshops:quenching_trough", {
 	description = S("Quenching Trough"),
-	tiles = {"image.png"},
+	tiles = {"default_furnace_top.png"},
 	groups = {workshops_forge = 1, oddly_breakable_by_hand = 1},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.1875, -0.5, 0.25, -0.0625, 0.5}, -- NodeBox1
+			{-0.1875, -0.3125, -0.5, 0.1875, -0.1875, 0.5}, -- NodeBox2
+			{-0.125, -0.4375, -0.5, 0.125, -0.3125, 0.5}, -- NodeBox3
+			{-0.25, -0.5, -0.5, -0.1875, -0.1875, -0.4375}, -- leg1
+			{-0.25, -0.5, 0.4375, -0.1875, -0.1875, 0.5}, -- leg2
+			{0.1875, -0.5, -0.5, 0.25, -0.1875, -0.4375}, -- leg3
+			{0.1875, -0.5, 0.4375, 0.25, -0.1875, 0.5}, -- leg4
+		}
+	}
 })
 
 minetest.register_node("workshops:metalworking_tool_rack", {
