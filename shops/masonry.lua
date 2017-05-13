db87499cc111f95ce936270e68dbce51ff694130
FaceDeer <derksenmobile@gmail.com>
2017-05-13 16:13:13 -0600

    add nodebox for slab support

--- shops/masonry.lua
+++ shops/masonry.lua
@@ -3,8 +3,22 @@ local S, NS = dofile(MP.."/intllib.lua")
 
 minetest.register_node("workshops:stone_support", {
 	description = S("Slab Support"),
-	tiles = {"image.png"},
+	tiles = {"default_wood.png"},
 	groups = {workshops_masonry = 2, oddly_breakable_by_hand = 1},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{0.1875, -0.5, 0.125, 0.5, 0.1875, 0.4375}, -- NodeBox1
+			{-0.5, -0.5, 0.125, -0.1875, 0.1875, 0.4375}, -- NodeBox2
+			{-0.5, 0.1875, 0.125, 0.5, 0.5, 0.4375}, -- NodeBox3
+			{-0.5, 0.1875, -0.4375, 0.5, 0.5, -0.125}, -- NodeBox4
+			{-0.5, -0.5, -0.4375, -0.1875, 0.1875, -0.125}, -- NodeBox5
+			{0.1875, -0.5, -0.4375, 0.5, 0.1875, -0.125}, -- NodeBox6
+		}
+	}
 })
 
 minetest.register_node("workshops:stoneworking_tool_rack", {
