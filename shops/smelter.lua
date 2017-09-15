b2c72669eb691602d18ee474276dfb81955326f5
FaceDeer <derksenmobile@gmail.com>
2017-09-14 20:40:13 -0600

    new smelter nodebox

--- shops/smelter.lua
+++ shops/smelter.lua
@@ -214,15 +214,28 @@ end
 
 minetest.register_node("workshops:smelting_furnace", {
 	description = S("Smelting Furnace"),
-	drawtype = "normal",
 	tiles = {
-		"default_furnace_top.png", "default_furnace_bottom.png",
-		"default_furnace_side.png", "default_furnace_side.png",
-		"default_furnace_side.png", "default_furnace_front.png"
+		"default_cobble.png"
 	},
 	paramtype2 = "facedir",
 	is_ground_content = false,
 	groups = {oddly_breakable_by_hand = 1, cracky=3, workshops_smelter = 3},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{0.125, -0.5, -0.5, 0.5, -0.1875, 0.5}, -- base1
+			{-0.4375, -0.1875, -0.4375, 0.4375, 0.0625, 0.4375}, -- layer2
+			{-0.375, 0.0625, -0.375, 0.375, 0.25, 0.375}, -- layer3
+			{-0.3125, 0.25, -0.3125, 0.3125, 0.375, 0.3125}, -- layer4
+			{-0.125, 0.4375, -0.125, 0.125, 0.5, 0.125}, -- layer6
+			{-0.25, 0.375, -0.25, 0.25, 0.4375, 0.25}, -- layer5
+			{-0.5, -0.5, -0.5, -0.125, -0.1875, 0.5}, -- base2
+			{-0.125, -0.5, 0.25, 0.125, -0.1875, 0.5}, -- base3
+			{-0.25, -0.1875, -0.5, 0.25, -0.0625, -0.4375}, -- arch
+		}
+	},
 
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
