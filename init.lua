cbecb30429d7863fe63e87aa82756ef26343da90
FaceDeer <derksenmobile@gmail.com>
2017-09-26 20:15:56 -0600

    get started on masonry shop

--- init.lua
+++ init.lua
@@ -9,9 +9,7 @@ dofile(MP.."/shops/masonry.lua")
 dofile(MP.."/shops/forge.lua")
 dofile(MP.."/shops/kitchen.lua")
 
-
 workshops.get_workshop_score = function(pos, radius, height, group_name, workshop_nodes)
-
 	local node_list = minetest.find_nodes_in_area(
 			{x=pos.x-radius, y=pos.y-height, z=pos.z-radius},
 			{x=pos.x+radius, y=pos.y+height, z=pos.z+radius},
@@ -27,84 +25,6 @@ workshops.get_workshop_score = function(pos, radius, height, group_name, worksho
 	end
 end
 
-
-
-local metal_ingots =
-{
-	['default:steel_ingot'] = true,
-	['default:copper_ingot'] = true,
-	['default:bronze_ingot'] = true,
-	["default:gold_ingot"] = true,
-	['moreores:mithril_ingot'] = true,
-	['moreores:tin_ingot'] = true,
-	['moreores:silver_ingot'] = true,
-	["technic:cast_iron_ingot"] = true,
-	["technic:stainless_steel_ingot"] = true,
-	["metals:pig_iron_ingot"] = true,
-	["metals:wrought_iron_ingot"] = true,
-	["metals:steel_ingot"] = true,
-}
-
-local function is_metal_input(item_name)
-	if minetest.get_item_group(item_name, "metal_ingot") > 0 then
-		return true
-	end
-	return metal_ingots[item_name]
-end
-
--- creates "recycling" recipes that allow items to be melted down for their metal content
--- doesn't otherwise disturb the crafting systems.
-simplecrafting_lib.register_recipe_import_filter(function(legacy_method, recipe)
-	if legacy_method ~= "normal" then 
-		return
-	end
-	
-	local metal_inputs = {}
-	local has_metal_inputs = false
-	for item, count in pairs(recipe.input) do
-		if is_metal_input(item) then
-			metal_inputs[item] = count
-			has_metal_inputs = true
-		end
-	end
-	
-	if has_metal_inputs then
-		local max_item
-		local max_count = 0
-		local total_count = 0
-		for item, count in pairs(metal_inputs) do
-			total_count = total_count + count
-			if count > max_count then
-				max_count = count
-				max_item = item
-			end
-		end
-		metal_inputs[max_item] = nil
-		local recycle_recipe = {}
-		recycle_recipe.cooktime = total_count * 3.0
-		recycle_recipe.input = {}
-		for item, count in pairs(recipe.output) do
-			recycle_recipe.input[item] = count
-		end
-		recycle_recipe.output = {[max_item] = max_count}
-		recycle_recipe.returns = metal_inputs
-		
-		simplecrafting_lib.register("smelter", recycle_recipe)
-	end
-end)
-
--- appropriates recipes that output metal ingots for the smelter
-simplecrafting_lib.register_recipe_import_filter(function(legacy_method, recipe)
-	if legacy_method ~= "cooking" then 
-		return
-	end
-	
-	for item, count in pairs(recipe.output) do
-		if is_metal_input(item) then
-			return "smelter", true
-		end
-	end
-end)
 
 simplecrafting_lib.import_legacy_recipes()
 
--- shops/masonry.lua
+++ shops/masonry.lua
@@ -1,7 +1,55 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
-minetest.register_node("workshops:stone_support", {
+local raw_stone = 
+{
+-- Commented out entries already belong to group "stone" in default.
+--	["default:stone"] = true
+--	["default:cobble"] = true
+--	["default:stonebrick"] = true
+--	["default:stone_block"] = true
+--	["default:mossycobble"] = true
+--	["default:desert_stone"] = true
+--	["default:desert_cobble"] = true
+--	["default:desert_stonebrick"] = true
+--	["default:desert_stone_block"] = true
+	["default:sandstone"] = true,
+	["default:sandstonebrick"] = true,
+	["default:sandstone_block"] = true,
+	["default:desert_sandstone"] = true,
+	["default:desert_sandstone_brick"] = true,
+	["default:desert_sandstone_block"] = true,
+	["default:silver_sandstone"] = true,
+	["default:silver_sandstone_brick"] = true,
+	["default:silver_sandstone_block"] = true,
+	["default:obsidian"] = true,
+	["default:obsidianbrick"] = true,
+	["default:obsidian_block"] = true,
+}
+
+local function is_stone_input(item_name)
+	if minetest.get_item_group(item_name, "stone") > 0 then
+		return true
+	end
+	return raw_stone[item_name]
+end
+
+-- appropriates recipes with stone inputs
+simplecrafting_lib.register_recipe_import_filter(function(legacy_method, recipe)
+	if legacy_method ~= "normal" then 
+		return
+	end
+	
+	for item, count in pairs(recipe.input) do
+		if is_stone_input(item) then
+			return "masonry", true
+		end
+	end
+end)
+
+local table_functions = simplecrafting_lib.generate_table_functions("masonry", true, false)
+
+local table_def = {
 	description = S("Slab Support"),
 	tiles = {"default_wood.png"},
 	groups = {workshops_masonry = 2, oddly_breakable_by_hand = 1},
@@ -19,7 +67,13 @@ minetest.register_node("workshops:stone_support", {
 			{0.1875, -0.5, -0.4375, 0.5, 0.1875, -0.125}, -- NodeBox6
 		}
 	}
-})
+}
+
+for k, v in pairs(table_functions) do
+	table_def[k] = v
+end
+
+minetest.register_node("workshops:stone_support", table_def)
 
 minetest.register_node("workshops:stoneworking_tool_rack", {
 	description = S("Stoneworking Tool Rack"),
